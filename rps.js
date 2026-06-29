10986b2db31ca073c52b9a1cc7fc57950c1bd13a
Tamanna Wadhwani <tamannatw14@gmail.com>
2026-06-29 18:30:40 +0530

    Add Rock Paper Scissors UI

--- rps.js
+++ rps.js
@@ -1,40 +1,63 @@
-console.log("Let's begin!");
+let userScore = 0;
+let compScore = 0;
 
-let playAgain = "yes"; // initial value
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
-while (playAgain.toLowerCase() === "yes") {
-    let userInput = prompt("Enter your choice:\nRock\nPaper\nScissor");
-    console.log("You chose:", userInput);
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 
-    let computerChoiceNum = Math.floor(Math.random() * 3) + 1;
-    let computerChoice;
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
 
-    if (computerChoiceNum === 1) {
-        computerChoice = "Rock";
-    } else if (computerChoiceNum === 2) {
-        computerChoice = "Paper";
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  //Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      //scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      //rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
     } else {
-        computerChoice = "Scissor";
+      //rock, paper
+      userWin = compChoice === "rock" ? false : true;
     }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
 
-    console.log("Computer chose:", computerChoice);
-
-    if (computerChoice === userInput) {
-        console.log("It's a Draw");
-    }
-    else if (
-        (userInput === "Rock" && computerChoice === "Scissor") ||
-        (userInput === "Paper" && computerChoice === "Rock") ||
-        (userInput === "Scissor" && computerChoice === "Paper")
-    ) {
-        console.log("YOU WON :)");
-    }
-    else {
-        console.log("Computer won :(");
-    }
-
-    // Ask if player wants to continue
-    playAgain = prompt("Do you want to play again? (yes/no)");
-}
-
-       
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
